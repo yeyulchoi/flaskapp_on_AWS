6bc03f486aea15f99cc978a9d66063bb74edf1f9
yeyulchoi <yeyulchoi@outlook.com>
2024-03-09 18:07:36 -0500

    added add_book functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import mysql.connector
 
@@ -24,9 +24,22 @@ class Book(db.Model):
 
 
 @app.route('/')
-def index():
+def home():
     books = Book.query.all()
     return render_template('index.html', books=books)
+
+@app.route('/add', methods=['POST'])
+def add_book():
+    if request.method =='POST':
+        book = Book(
+            title=request.form.get('title'),
+            author=request.form.get('author'),
+            price=request.form.get('price'))
+        db.session.add(book)
+        db.session.commit()
+        return redirect(url_for('home'))
+
+
 
 if __name__ =='__main__':
     with app.app_context():
